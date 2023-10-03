0ee45eb75e6aec6c2e97d662769650e2714dca58
chimekinglsey <onlinekingsley@gmail.com>
2023-10-03 23:51:15 +0100

    Unlock the box challenge (right and wrong thinking)

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,22 +2,15 @@
 """
 Technical Interview Challenge: Checks if a key can unlock all the boxes
 """
-def canUnlockAll(boxes):
-    """A key with same number as the box opens that box"""
-    checkList = []
-    if len(boxes) > 0:
-        checkList.append(boxes[0]) #this is unlocked already
-        for box in boxes:
-            if len(box) == 0:
-                 checkList.append(box)#checks each box inside the boxes
-            for number in box:
-                if boxes[number]:#unlocks a box
-                    for item in boxes[number]:
-                        if item != number:
-                            if not boxes[number] in checkList:
-                                checkList.append(boxes[number])
 
-    print("checklist: ", checkList, '\nBoxes: ', boxes)
-    if len(checkList) == len(boxes):
-        return True
-    return False
+
+def canUnlockAll(boxes):
+    # Start with the first box, which is unlocked by default
+    checkList = [0]
+
+    for box_index in checkList:
+        for key in boxes[box_index]:
+            if key < len(boxes) and key not in checkList:
+                checkList.append(key)
+
+    return len(checkList) == len(boxes)
