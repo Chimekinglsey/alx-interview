001199a880d98fa13d899bf735cd40271e228b21
chimekinglsey <onlinekingsley@gmail.com>
2023-10-20 01:27:24 +0100

    Parse log. An interview question to read from continuosly parse it

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,56 +1,52 @@
 #!/usr/bin/python3
-"""log parsing"""
+"""
+This module contains a method that reads stdin line by line and
+computes metrics
+"""
+
 import sys
-import re
-from time import sleep
 
 
-def match(x: str) -> bool:
-    """Checks for input match"""
-    ip_match = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - '
-    date_match = r'\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6}\] '
-    stats_match = r'"GET /projects/260 HTTP/1\.1" \d{3} \d+'
-    pattern = ip_match + date_match + stats_match
-    return True if re.match(pattern, x) else False
+def display_metrics(total_size, status_code):
+    """
+    Function that print the metrics
+    """
 
-
-def log_parse() -> None:
-    """ A function that reads stdin line by line and computes metrics """
-    while True:
-        usage = '<IP Address> - [<date>] "GET /projects/260 HTTP/1.1"\
-<status code> <file size>'
-        line_count = 0
-        total_f_size = 0
-        status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
-        status_code_dict = {'200': 0, '301': 0, '400': 0, '401': 0,
-                            '403': 0, '404': 0, '405': 0, '500': 0}
-        # sorted_s_code = sorted(status_code_dict)
-        try:
-            for line in sys.stdin:
-                if match(line):
-                    a, b, c, d, e, f, g, status_code, f_size = line.split()
-                    total_f_size += int(f_size)
-                    if status_code in str(status_codes):
-                        for k, v in status_code_dict.items():
-                            if k == status_code:
-                                status_code_dict[status_code] += 1
-                    line_count += 1
-                    if line_count % 10 == 0:
-                        print(f'File size: {total_f_size}')
-                        for key, val in status_code_dict.items():
-                            if val > 0:
-                                print(f"{key}: {val}")
-
-                else:
-                    raise TypeError(usage)
-
-        except KeyboardInterrupt:
-            print(f'File size: {total_f_size}')
-            for key, val in status_code_dict.items():
-                if val > 0:
-                    print(f"{key}: {val}")
-            exit(1)
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(status_code.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
 
 
 if __name__ == '__main__':
-    log_parse()
+    total_size = 0
+    status_code = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0
+    }
+
+    try:
+        i = 0
+        for line in sys.stdin:
+            args = line.split()
+            if len(args) > 6:
+                status = args[-2]
+                file_size = args[-1]
+                total_size += int(file_size)
+                if status in status_code:
+                    i += 1
+                    status_code[status] += 1
+                    if i % 10 == 0:
+                        display_metrics(total_size, status_code)
+
+    except KeyboardInterrupt:
+        display_metrics(total_size, status_code)
+        raise
+    else:
+        display_metrics(total_size, status_code)
