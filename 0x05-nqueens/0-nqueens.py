57291469150bee34c21cbb85308de7c31ca58b99
chimekinglsey <onlinekingsley@gmail.com>
2023-11-03 02:59:26 +0100

    chess game - leading queens challenge

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,51 +1,87 @@
 #!/usr/bin/python3
-"""N-Queens Solution Finder"""
+"""N queens solution finder module.
+"""
 import sys
 
-solutions = []  # List of possible solutions to the N-Queens problem
-n = 0  # Size of the chessboard
-pos = None  # List of possible positions on the chessboard
+
+solutions = []
+"""The list of possible solutions to the N queens problem.
+"""
+n = 0
+"""The size of the chessboard.
+"""
+pos = None
+"""The list of possible positions on the chessboard.
+"""
 
 
 def get_input():
-    """Retrieve and validate the program's argument.
-    Returns the size of the chessboard."""
+    """Retrieves and validates this program's argument.
+
+    Returns:
+        int: The size of the chessboard.
+    """
     global n
+    n = 0
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-
     try:
         n = int(sys.argv[1])
-    except ValueError:
+    except Exception:
         print("N must be a number")
         sys.exit(1)
-
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-
     return n
 
 
 def is_attacking(pos0, pos1):
-    """Check if two queens' positions are in an attacking mode.
-    Returns True if they are."""
-    return (pos0[0] == pos1[0]) or (pos0[1] == pos1[1]) or (
-        abs(pos0[0] - pos1[0]) == abs(pos0[1] - pos1[1]))
+    """Checks if the positions of two queens are in an attacking mode.
+
+    Args:
+        pos0 (list or tuple): The first queen's position.
+        pos1 (list or tuple): The second queen's position.
+
+    Returns:
+        bool: True if the queens are in an attacking position else False.
+    """
+    if (pos0[0] == pos1[0]) or (pos0[1] == pos1[1]):
+        return True
+    return abs(pos0[0] - pos1[0]) == abs(pos0[1] - pos1[1])
 
 
 def group_exists(group):
-    """Check if a group exists in the list of solutions.
-    Returns True if it exists."""
+    """Checks if a group exists in the list of solutions.
+
+    Args:
+        group (list of integers): A group of possible positions.
+
+    Returns:
+        bool: True if it exists, otherwise False.
+    """
     global solutions
-    return any(all(pos in stn for pos in group) for stn in solutions)
+    for stn in solutions:
+        i = 0
+        for stn_pos in stn:
+            for grp_pos in group:
+                if stn_pos[0] == grp_pos[0] and stn_pos[1] == grp_pos[1]:
+                    i += 1
+        if i == n:
+            return True
+    return False
 
 
 def build_solution(row, group):
-    """Build a solution for the N-Queens problem."""
-    global solutions, n
+    """Builds a solution for the n queens problem.
 
+    Args:
+        row (int): The current row in the chessboard.
+        group (list of lists of integers): The group of valid positions.
+    """
+    global solutions
+    global n
     if row == n:
         tmp0 = group.copy()
         if not group_exists(tmp0):
@@ -53,18 +89,19 @@ def build_solution(row, group):
     else:
         for col in range(n):
             a = (row * n) + col
-            used_positions = any(is_attacking(pos[a], pos_b)
-                                 for pos_b in group)
-            if not used_positions:
-                group.append(pos[a])
+            matches = zip(list([pos[a]]) * len(group), group)
+            used_positions = map(lambda x: is_attacking(x[0], x[1]), matches)
+            group.append(pos[a].copy())
+            if not any(used_positions):
                 build_solution(row + 1, group)
-                group.pop()
+            group.pop(len(group) - 1)
 
 
 def get_solutions():
-    """Get solutions for the given chessboard size."""
+    """Gets the solutions for the given chessboard size.
+    """
     global pos, n
-    pos = [(x // n, x % n) for x in range(n * n)]
+    pos = list(map(lambda x: [x // n, x % n], range(n ** 2)))
     a = 0
     group = []
     build_solution(a, group)
