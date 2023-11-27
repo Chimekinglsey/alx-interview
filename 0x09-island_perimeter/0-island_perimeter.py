7b1bb0547fc12489bf5e4559e11ec00d9ddf7559
chimekinglsey <onlinekingsley@gmail.com>
2023-11-27 15:48:13 +0100

    Island perimeter calculator

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,30 +1,15 @@
-#!/usr/bin/python3
-"""Island Perimeter Calculator
-"""
-
-
 def island_perimeter(grid):
     """Returns the perimeter of the island described in `grid`"""
-    grid_extract = []
-    max_width = 0
+    perimeter = 0
 
-    for row in grid:
-        firs_land_idx = None
-        last_land_idx = None
-        cell_index = -1
-        for cell in row:
-            cell_index += 1
-            if cell == 1:
-                if firs_land_idx is None:
-                    firs_land_idx = cell_index
-                else:
-                    last_land_idx = cell_index
-            if firs_land_idx:
-                if last_land_idx is None:
-                    last_land_idx = 0
-                land_row = [i for i in range(firs_land_idx, last_land_idx + 1)]
-                if land_row and len(land_row) > 0:
-                    if len(land_row) > max_width:
-                        max_width = len(land_row)
-                    grid_extract.append(land_row)
-    return (len(grid_extract) * max_width)
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
---
+++ 0x09-island_perimeter/test2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Island Perimeter Calculator
+"""
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in `grid`"""
+    grid_extract = []
+    max_width = 0
+
+    for row in grid:
+        cell_index = -1
+        firs_land_idx, last_land_idx = None, None
+        for cell in row:
+            cell_index += 1
+            if cell == 1:
+                if firs_land_idx is None:
+                    firs_land_idx = cell_index
+                else:
+                    last_land_idx = cell_index
+            if firs_land_idx:
+                if last_land_idx is None:
+                    last_land_idx = 0
+                land_row = [i for i in range(firs_land_idx, last_land_idx + 1)]
+                if land_row and len(land_row) > 0:
+                    if len(land_row) > max_width:
+                        max_width = len(land_row)
+                    grid_extract.append(land_row)
+    return (len(grid_extract) * max_width)
